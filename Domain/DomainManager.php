529dd144a2ce6c3fe72e9686ec7359503d30917c
François Pluchino <francois.pluchino@sonatra.com>
2015-03-28 13:24:58 +0100

    Allow use doctrine shortcut of class name

--- Domain/DomainManager.php
+++ Domain/DomainManager.php
@@ -12,6 +12,7 @@
 namespace Sonatra\Bundle\ResourceBundle\Domain;
 
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ObjectManager;
 use Sonatra\Bundle\DefaultValueBundle\DefaultValue\ObjectFactoryInterface;
 use Sonatra\Bundle\ResourceBundle\Exception\InvalidArgumentException;
 use Symfony\Component\EventDispatcher\Event;
@@ -45,6 +46,11 @@ class DomainManager implements DomainManagerInterface
     protected $of;
 
     /**
+     * @var array
+     */
+    protected $cache;
+
+    /**
      * Constructor.
      *
      * @param DomainInterface[]        $domains The resource domains
@@ -59,6 +65,7 @@ class DomainManager implements DomainManagerInterface
         $this->or = $or;
         $this->ed = $ed;
         $this->of = $of;
+        $this->cache = array();
 
         foreach ($domains as $domain) {
             $this->add($domain);
@@ -105,10 +112,40 @@ class DomainManager implements DomainManagerInterface
      */
     public function get($class)
     {
+        if (isset($this->cache[$class])) {
+            return $this->domains[$this->cache[$class]];
+        }
+
+        $getClass = $class;
+        $manager = $this->getManager($class);
+        $class = $manager->getClassMetadata($class)->getName();
+
         if ($this->has($class)) {
+            $this->cache[$getClass] = $class;
+
             return $this->domains[$class];
         }
 
         throw new InvalidArgumentException(sprintf('The resource domain for "%s" class is not managed', $class));
+    }
+
+    /**
+     * Get the doctrine object manager of the class.
+     *
+     * @param string $class The class name or doctrine shortcut class name
+     *
+     * @return ObjectManager
+     *
+     * @throws InvalidArgumentException When the class is not registered in doctrine
+     */
+    protected function getManager($class)
+    {
+        $manager = $this->or->getManagerForClass($class);
+
+        if (null !== $manager) {
+            return $manager;
+        }
+
+        throw new InvalidArgumentException(sprintf('The "%s" class is not registered in doctrine', $class));
     }
 }
