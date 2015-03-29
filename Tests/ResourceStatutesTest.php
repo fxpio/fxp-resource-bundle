53279874021f26a8c2808f8a04a5a8bf09472c4b
François Pluchino <francois.pluchino@sonatra.com>
2015-03-29 18:23:19 +0200

    Add resource statutes tests

---
+++ Tests/ResourceStatutesTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\ResourceBundle\Tests;
+
+use Sonatra\Bundle\ResourceBundle\ResourceStatutes;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class ResourceStatutesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstantiationOfClass()
+    {
+        $this->setExpectedException('Sonatra\Bundle\ResourceBundle\Exception\ClassNotInstantiableException');
+        new ResourceStatutes();
+    }
+}
