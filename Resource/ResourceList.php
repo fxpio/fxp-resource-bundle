d10755bfc85a447aef610e91d6b098fa083fbaa0
François Pluchino <francois.pluchino@sonatra.com>
2015-04-09 10:02:36 +0200

    Split resource list class

--- Resource/ResourceList.php
+++ Resource/ResourceList.php
@@ -11,141 +11,22 @@
 
 namespace Sonatra\Bundle\ResourceBundle\Resource;
 
-use Sonatra\Bundle\ResourceBundle\Exception\OutOfBoundsException;
 use Sonatra\Bundle\ResourceBundle\ResourceStatutes;
 use Sonatra\Bundle\ResourceBundle\ResourceListStatutes;
-use Symfony\Component\Form\FormErrorIterator;
-use Symfony\Component\Validator\ConstraintViolationList;
-use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
  * Resource list.
  *
  * @author François Pluchino <francois.pluchino@sonatra.com>
  */
-class ResourceList implements \IteratorAggregate, ResourceListInterface
+class ResourceList extends AbstractResourceList
 {
     /**
-     * @var string
-     */
-    protected $status;
-
-    /**
-     * @var ResourceInterface[]
-     */
-    protected $resources;
-
-    /**
-     * @var ConstraintViolationListInterface
-     */
-    protected $errors;
-
-    /**
-     * @var ConstraintViolationListInterface|FormErrorIterator[]|null
-     */
-    protected $childrenErrors;
-
-    /**
-     * Constructor.
-     *
-     * @param ResourceInterface[]              $resources The list of resource
-     * @param ConstraintViolationListInterface $errors    The list of errors
-     */
-    public function __construct(array $resources = array(), ConstraintViolationListInterface $errors = null)
-    {
-        $this->resources = array();
-        $this->errors = null !== $errors ? $errors : new ConstraintViolationList();
-
-        foreach ($resources as $resource) {
-            $this->add($resource);
-        }
-    }
-
-    /**
      * {@inheritdoc}
      */
-    public function getStatus()
+    public function getIterator()
     {
-        if (null === $this->status) {
-            $this->refreshStatus();
-        }
-
-        return $this->status;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getResources()
-    {
-        return $this->resources;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function add(ResourceInterface $resource)
-    {
-        $this->reset();
-        $this->resources[] = $resource;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function addAll(ResourceListInterface $otherList)
-    {
-        $this->reset();
-
-        foreach ($otherList as $resource) {
-            $this->resources[] = $resource;
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function all()
-    {
-        return $this->resources;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function get($offset)
-    {
-        if (!isset($this->resources[$offset])) {
-            throw new OutOfBoundsException(sprintf('The offset "%s" does not exist.', $offset));
-        }
-
-        return $this->resources[$offset];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function has($offset)
-    {
-        return isset($this->resources[$offset]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function set($offset, ResourceInterface $resource)
-    {
-        $this->reset();
-        $this->resources[$offset] = $resource;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function remove($offset)
-    {
-        $this->reset();
-        unset($this->resources[$offset]);
+        return new \ArrayIterator($this->resources);
     }
 
     /**
@@ -176,67 +57,6 @@ class ResourceList implements \IteratorAggregate, ResourceListInterface
 
     /**
      * {@inheritdoc}
-     */
-    public function getIterator()
-    {
-        return new \ArrayIterator($this->resources);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function count()
-    {
-        return count($this->resources);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetExists($offset)
-    {
-        return $this->has($offset);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetGet($offset)
-    {
-        return $this->get($offset);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetSet($offset, $resource)
-    {
-        if (null === $offset) {
-            $this->add($resource);
-        } else {
-            $this->set($offset, $resource);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetUnset($offset)
-    {
-        $this->remove($offset);
-    }
-
-    /**
-     * Reset the value of status and children errors.
-     */
-    protected function reset()
-    {
-        $this->status = null;
-        $this->childrenErrors = null;
-    }
-
-    /**
-     * Refresh the status of this list.
      */
     protected function refreshStatus()
     {
